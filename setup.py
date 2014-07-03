aaa43366b41ab81f7ccf0a3dd55363be98248514
Tsung-Han Yang <blacksburg98@yahoo.com>
2014-07-03 16:23:54 -0700

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dyplot',
-    version='0.6.000',
+    version='0.6.001',
     author='Tsung-Han Yang',
     author_email='blacksburg98@yahoo.com',
     packages=['dyplot'],
