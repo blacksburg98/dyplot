8980a46cdc70f4ff3e77d1ea148af2815f541480
Tsung-Han Yang <blacksburg98@yahoo.com>
2014-11-30 09:46:43 -0800

    change README.md to README.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import os
 from setuptools import setup, find_packages
 
-#__currdir__ = os.getcwd()
-#__readme__ = os.path.join(__currdir__, 'README.md')
+__currdir__ = os.getcwd()
+__readme__ = os.path.join(__currdir__, 'README.txt')
 setup(
     name='dyplot',
-    version='0.7.005',
+    version='0.7.006',
     author='Tsung-Han Yang',
     author_email='blacksburg98@yahoo.com',
     packages=find_packages(),
     url='http://pypi.python.org/pypi/dyplot/',
     license='LICENSE.txt',
     description='matplotlib-like plot functions for dygraphs.js and c3.js.',
-#    long_description = open(__readme__).read(),
-#    long_description=long_description, 
+    long_description = open(__readme__).read(),
     install_requires=[
         "pandas >= 0.7.3",
     ],
