f24be4baaea965f14acc25fcb146431d1143303f
Tsung-Han Yang <blacksburg98@yahoo.com>
2015-11-16 17:31:56 -0800

    fix

--- examples/nvd3_scatter.py
+++ examples/nvd3_scatter.py
@@ -4,11 +4,11 @@ from dyplot.scatter import Scatter
 if __name__ == '__main__':
     x = np.random.rand(10)
     y = np.random.rand(10)
-    s = Scatter(x, y, "Group 1", s=2)
+    s = Scatter(x, y, "Group 1")
     x = np.random.rand(10)
     y = np.random.rand(10)
     s(x, y, "Group 2", marker="+", s=[1,3,2,4,5,6,6,5,4,3])
     x = np.random.rand(10)
     y = np.random.rand(10)
-    s(x, y, "Group 3", marker=["^", 'v', 'o', '+', 's', 's', 's', 'D', 'D', 'v'])
+    s(x, y, "Group 3", s=2, marker=["^", 'v', 'o', '+', 's', 's', 's', 'D', 'D', 'v'])
     s.savefig(csv_file="nvd3_scatter.csv",html_file="nvd3_scatter.html")
