05c07669043d5454ff7638edef372e5b60f20152
Tsung-Han Yang <blacksburg98@yahoo.com>
2015-11-18 07:07:25 -0800

    change group 2

--- examples/nvd3_scatter.py
+++ examples/nvd3_scatter.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     s = Scatter(x, y, "Group 1", s=2, slope=-1, intercept=0.9)
     x = np.random.rand(10)
     y = np.random.rand(10)
-    s(x, y, "Group 2", marker="+", s=[1,3,2,4,5,6,6,5,4,3])
+    s(x, y, "Group 2", marker="+", s=[1,3,2,4,5,6,6,5,4,3], slope=1, intercept=0.1)
     x = np.random.rand(10)
     y = np.random.rand(10)
     s(x, y, "Group 3", s=2, marker=["^", 'v', 'o', '+', 's', 's', 's', 'D', 'D', 'v'])
